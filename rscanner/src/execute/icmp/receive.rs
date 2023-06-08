32ef1cede326b3081143a49dcb8e299d81bdf6b4
gongzhengyang <gongzhengyang@bolean.com.cn>
2023-06-08 13:40:58 +0800

    fix: set bigger value for receive icmp packets for less warn log

--- rscanner/src/execute/icmp/receive.rs
+++ rscanner/src/execute/icmp/receive.rs
@@ -10,7 +10,7 @@ pub async fn receive_packets() -> anyhow::Result<()> {
     let (_, mut rx) = common::get_transport_channel()?;
     let mut iter = pnet_transport::icmp_packet_iter(&mut rx);
     loop {
-        tokio::time::sleep(Duration::from_nanos(1)).await;
+        tokio::time::sleep(Duration::from_micros(10)).await;
         if let Ok(Some((packet, addr))) = iter.next_with_timeout(Duration::from_millis(1)) {
             if monitor::is_addr_received(&addr).await {
                 continue;
